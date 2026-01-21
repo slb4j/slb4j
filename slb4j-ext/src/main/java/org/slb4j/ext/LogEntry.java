327ddcdd3f07f80d87b9c2e0b95891bf83e8d219
Axel Howind <axh@dua3.com>
2026-01-21 14:06:59 +0100

    Javadoc

--- slb4j-ext/src/main/java/org/slb4j/ext/LogEntry.java
+++ slb4j-ext/src/main/java/org/slb4j/ext/LogEntry.java
@@ -98,20 +98,19 @@ public interface LogEntry {
     @Nullable Throwable throwable();
 
     /**
+     * Creates a new instance of a {@link LogEntry} with the specified properties.
      *
-     * @param time        The timestamp of when the log event occurred.
-     * @param logger      The name of the logger that generated the log event.
-     * @param level       The severity level of the log event, represented by {@link LogLevel}.
-     * @param marker      An optional marker associated with the log event, providing additional categorization.
-     * @param mdc         An optional Mapping Diagnostic Context (MDC) associated with the log event, providing
-     *                    contextual information for diagnostic purposes.
-     * @param location    An optional {@link Location} object providing details about the origin of the log event
-     *                    within the code (e.g., class name, method name, file name, line number).
-     * @param message     A supplier of the log message. The message is lazily evaluated, allowing computation
-     *                    to be deferred until it is actually needed.
-     * @param throwable   An optional throwable associated with the log event, representing an exception or error
-     *                    related to the event.
-     * @return an instance of LogEntry representing the log event with the given data.
+     * @param time      the timestamp of the log event in milliseconds since the epoch.
+     * @param logger    the name of the logger that generated the log event.
+     * @param level     the severity level of the log event, represented as a {@link LogLevel}.
+     * @param marker    an optional marker associated with the log event, or null if no marker is present.
+     * @param mdc       the Mapping Diagnostic Context (MDC) containing key-value pairs of contextual
+     *                  information, or null if no MDC is associated with the log event.
+     * @param location  the location information of the log event, such as the class, method,
+     *                  file, and line number, or null if unavailable.
+     * @param message   the textual message associated with the log event, or null if no message is provided.
+     * @param throwable the throwable (e.g., an exception) associated with the log event, or null if none is present.
+     * @return a new {@link LogEntry} instance representing the log event.
      */
     static LogEntry of(
             long time,
