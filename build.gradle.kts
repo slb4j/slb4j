263dbf74d210d317fc8d6eb56e49cc075254feaa
Axel Howind <axh@dua3.com>
2026-01-23 02:02:55 +0100

    Update project version to `0.1-rc`.

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,7 +38,7 @@ java {
 /////////////////////////////////////////////////////////////////////////////
 
 object Meta {
-    const val VERSION = "0.1-beta3-SNAPSHOT"
+    const val VERSION = "0.1-rc"
     const val DESCRIPTION = "Simple Logging Backend for Java"
     const val INCEPTION_YEAR = "2026"
     const val GROUP = "org.slb4j"
