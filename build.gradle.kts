7e33bd23ca02db7d9e1afb1ad034e66e69554d66
xzel23 <axel@dua3.com>
2026-01-30 06:23:40 +0000

    Prepare for next development cycle: 0.2-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,7 +38,7 @@ java {
 /////////////////////////////////////////////////////////////////////////////
 
 object Meta {
-    const val VERSION = "0.1"
+    const val VERSION = "0.2-SNAPSHOT"
     const val DESCRIPTION = "Simple Logging Backend for Java"
     const val INCEPTION_YEAR = "2026"
     const val GROUP = "org.slb4j"
