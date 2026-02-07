5aaa461acd84d78b65e6ddbc06348c0308100bf9
Axel Howind <axh@dua3.com>
2026-02-07 09:20:37 +0100

    Remove `Log4j2PropertiesTest` and streamline `ConfigParserLog4jTest` with expanded test cases

--- src/test/java/org/slb4j/config/ConfigParserLog4jTest.java
+++ src/test/java/org/slb4j/config/ConfigParserLog4jTest.java
@@ -2,22 +2,22 @@ package org.slb4j.config;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.slb4j.LayoutConfigurable;
 import org.slb4j.LogFilter;
 import org.slb4j.LogHandler;
 import org.slb4j.LogLevel;
 import org.slb4j.LoggingConfiguration;
 import org.slb4j.filter.CombinedFilter;
 import org.slb4j.filter.LogLevelFilter;
-import org.slb4j.filter.MarkerFilter;
 import org.slb4j.handler.ConsoleHandler;
 import org.slb4j.handler.FileHandler;
+import org.slb4j.handler.RotatingFileHandler;
 import org.slb4j.layout.PatternLayout;
 import org.slb4j.layout.SimpleLayout;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
 import java.util.Properties;
 import java.util.function.Consumer;
 import java.util.stream.Stream;
@@ -49,19 +49,6 @@ class ConfigParserLog4jTest {
 
         if (testCase.validate() != null) {
             testCase.validate().accept(result);
-        }
-
-        assertTrue(!result.getHandlers().isEmpty(), "Handlers should be parsed for " + testCase.name());
-        // Basic verification:  that at least some handlers have a layout
-        if (testCase.properties().contains("layout")) {
-            assertTrue(result.getHandlers().values().stream()
-                    .filter(LayoutConfigurable.class::isInstance)
-                    .anyMatch(h -> ((LayoutConfigurable) h).getLayout() != null), "Layout should be present for " + testCase.name());
-        }
-        // Basic verification:  that at least some handlers have a filter
-        if (testCase.properties().contains("filter") || testCase.properties().contains("filters")) {
-            assertTrue(result.getHandlers().values().stream()
-                    .anyMatch(h -> h.getFilter() != null), "Filter should be present for " + testCase.name());
         }
     }
 
@@ -172,6 +159,248 @@ class ConfigParserLog4jTest {
 
                             LogHandler consoleHandler = config.getHandlers().get("console");
                             assertNotNull(consoleHandler);
+                        }
+                ),
+                new TestCase("Basic Console", """
+                        appender.console.type = Console
+                        appender.console.name = STDOUT
+                        appender.console.layout.type = PatternLayout
+                        appender.console.layout.pattern = %d{HH:mm:ss.SSS} [%t] %-5level %logger{36} - %msg%n
+                        rootLogger.level = debug
+                        rootLogger.appenderRef.stdout.ref = STDOUT
+                        """,
+                        config -> {
+                            LogHandler handler = config.getHandler("console");
+                            assertNotNull(handler);
+                            assertInstanceOf(ConsoleHandler.class, handler);
+                            assertEquals("STDOUT", handler.name());
+                            PatternLayout layout = (PatternLayout) ((ConsoleHandler) handler).getLayout();
+                            assertEquals(PatternLayout.parseLog4jPattern("%d{HH:mm:ss.SSS} [%t] %-5level %logger{36} - %msg%n").getText(), layout.getText());
+
+                            LogFilter rootFilter = config.getRootFilter();
+                            assertInstanceOf(LogLevelFilter.class, rootFilter);
+                            assertTrue(rootFilter.isLevelEnabled(LogLevel.DEBUG));
+                        }
+                ),
+                new TestCase("Basic File", """
+                        appender.file.type = File
+                        appender.file.name = File
+                        appender.file.fileName = build/test.log
+                        appender.file.layout.type = PatternLayout
+                        appender.file.layout.pattern = %d %p %C{1.} [%t] %m%n
+                        rootLogger.level = info
+                        rootLogger.appenderRef.file.ref = File
+                        """,
+                        config -> {
+                            LogHandler handler = config.getHandler("file");
+                            assertNotNull(handler);
+                            assertInstanceOf(FileHandler.class, handler);
+                            assertEquals("File", handler.name());
+                            assertEquals(Path.of("build/test.log").toAbsolutePath(), ((FileHandler) handler).getPath().toAbsolutePath());
+
+                            LogFilter rootFilter = config.getRootFilter();
+                            assertInstanceOf(LogLevelFilter.class, rootFilter);
+                            assertTrue(rootFilter.isLevelEnabled(LogLevel.INFO));
+                            assertTrue(!rootFilter.isLevelEnabled(LogLevel.DEBUG));
+                        }
+                ),
+                new TestCase("Rolling File", """
+                        appender.rolling.type = RollingFile
+                        appender.rolling.name = RollingFile
+                        appender.rolling.fileName = build/rolling.log
+                        appender.rolling.filePattern = build/rolling-%d{MM-dd-yy-HH-mm-ss}-%i.log.gz
+                        appender.rolling.layout.type = PatternLayout
+                        appender.rolling.layout.pattern = %d %p %C{1.} [%t] %m%n
+                        appender.rolling.policies.type = Policies
+                        appender.rolling.policies.size.type = SizeBasedTriggeringPolicy
+                        appender.rolling.policies.size.size = 100MB
+                        appender.rolling.strategy.max = 5
+                        rootLogger.level = info
+                        rootLogger.appenderRef.rolling.ref = RollingFile
+                        """,
+                        config -> {
+                            LogHandler handler = config.getHandler("rolling");
+                            assertNotNull(handler);
+                            assertInstanceOf(RotatingFileHandler.class, handler);
+                            RotatingFileHandler rfh = (RotatingFileHandler) handler;
+                            assertEquals("RollingFile", rfh.name());
+                            assertEquals(Path.of("build/rolling.log").toAbsolutePath(), rfh.getPath().toAbsolutePath());
+                            assertEquals("build/rolling-%d{MM-dd-yy-HH-mm-ss}-%i.log.gz", rfh.getFilePattern());
+                            assertEquals(100L * 1024 * 1024, rfh.getMaxFileSize());
+                            assertEquals(5, rfh.getMaxBackupIndex());
+                        }
+                ),
+                new TestCase("Multiple Appenders", """
+                        appender.console.type = Console
+                        appender.console.name = STDOUT
+                        appender.file.type = File
+                        appender.file.name = FILE
+                        appender.file.fileName = build/test.log
+                        rootLogger.level = debug
+                        rootLogger.appenderRef.stdout.ref = STDOUT
+                        rootLogger.appenderRef.file.ref = FILE
+                        """,
+                        config -> {
+                            assertEquals(2, config.getHandlers().size());
+                            assertNotNull(config.getHandler("console"));
+                            assertNotNull(config.getHandler("file"));
+                            assertEquals(LogLevelFilter.class, config.getRootFilter().getClass());
+                            assertTrue(config.getRootFilter().isLevelEnabled(LogLevel.DEBUG));
+                        }
+                ),
+                new TestCase("Logger levels", """
+                        appender.console.type = Console
+                        appender.console.name = STDOUT
+                        logger.app.name = org.apache.logging.log4j.test1
+                        logger.app.level = debug
+                        logger.app.additivity = false
+                        logger.app.appenderRef.stdout.ref = STDOUT
+                        rootLogger.level = info
+                        rootLogger.appenderRef.stdout.ref = STDOUT
+                        """,
+                        config -> {
+                            LogHandler console = config.getHandlers().get("console");
+                            assertNotNull(console);
+                            LogFilter appFilter = console.getFilter();
+                            assertNotNull(appFilter);
+                            assertTrue(appFilter.isLevelEnabled(LogLevel.DEBUG));
+
+                            LogFilter rootFilter = config.getRootFilter();
+                            assertTrue(rootFilter.isLevelEnabled(LogLevel.INFO));
+                            assertTrue(!rootFilter.isLevelEnabled(LogLevel.DEBUG));
+                        }
+                ),
+                new TestCase("Baeldung - 6. Syntax of the log4j2.properties", """
+                        # The root logger with appender name
+                        rootLogger = DEBUG, STDOUT
+                        
+                        # Assign STDOUT a valid appender & define its layout 
+                        appender.console.name = STDOUT
+                        appender.console.type = Console
+                        appender.console.layout.type = PatternLayout
+                        appender.console.layout.pattern = %msg%n
+                        """,
+                        config -> {
+                            LogHandler handler = config.getHandler("console");
+                            assertNotNull(handler);
+                            assertEquals("STDOUT", handler.name());
+                            assertTrue(config.getRootFilter().isLevelEnabled(LogLevel.DEBUG));
+                        }
+                ),
+                new TestCase("Baeldung - 7.2 Console Logging", """
+                        # Root Logger
+                        rootLogger=DEBUG, STDOUT
+                        
+                        # Direct log messages to stdout
+                        appender.console.type = Console
+                        appender.console.name = STDOUT
+                        appender.console.layout.type = PatternLayout
+                        appender.console.layout.pattern = [%-5level] %d{yyyy-MM-dd HH:mm:ss.SSS} [%t] %C{1} - %msg%n
+                        """,
+                        config -> {
+                            LogHandler handler = config.getHandler("console");
+                            assertNotNull(handler);
+                            assertTrue(config.getRootFilter().isLevelEnabled(LogLevel.DEBUG));
+                        }
+                ),
+                new TestCase("Baeldung - 7.3. Multiple Destinations", """
+                        # Root Logger
+                        rootLogger=INFO, STDOUT, LOGFILE
+                        
+                        # Direct log messages to STDOUT
+                        appender.console.type = Console
+                        appender.console.name = STDOUT
+                        appender.console.layout.type = PatternLayout
+                        appender.console.layout.pattern = [%-5level] %d{yyyy-MM-dd HH:mm:ss.SSS} [%t] %C{1} - %msg%n
+                        
+                        # Direct to a file
+                        appender.file.type = File
+                        appender.file.name = LOGFILE
+                        appender.file.fileName = build/tmp/baeldung/logs/log4j2.log
+                        appender.file.layout.type = PatternLayout
+                        appender.file.layout.pattern = [%-5level] %d{yyyy-MM-dd HH:mm:ss.SSS} [%t] %C{1} - %msg%n
+                        appender.file.filter.threshold.type = ThresholdFilter
+                        appender.file.filter.threshold.level = info
+                        """,
+                        config -> {
+                            LogHandler fileHandler = config.getHandler("file");
+                            assertNotNull(fileHandler.getFilter());
+                            assertTrue(fileHandler.getFilter().isLevelEnabled(LogLevel.INFO));
+                        }
+                ),
+                new TestCase("JSON Layout", """
+                        appender.console.type = Console
+                        appender.console.name = STDOUT
+                        appender.console.layout.type = JsonLayout
+                        rootLogger.level = debug
+                        rootLogger.appenderRef.stdout.ref = STDOUT
+                        """,
+                        config -> {
+                            LogHandler handler = config.getHandler("console");
+                            assertNotNull(handler);
+                            assertInstanceOf(ConsoleHandler.class, handler);
+                            assertEquals("JsonLayout", ((ConsoleHandler) handler).getLayout().getType());
+                        }
+                ),
+                new TestCase("Custom Filter", """
+                        appender.console.type = Console
+                        appender.console.name = STDOUT
+                        appender.console.filter.threshold.type = ThresholdFilter
+                        appender.console.filter.threshold.level = debug
+                        rootLogger.level = debug
+                        rootLogger.appenderRef.stdout.ref = STDOUT
+                        """,
+                        config -> {
+                            LogHandler handler = config.getHandler("console");
+                            assertNotNull(handler.getFilter());
+                            assertTrue(handler.getFilter().isLevelEnabled(LogLevel.DEBUG));
+                        }
+                ),
+                new TestCase("Simple Layout", """
+                        appender.console.type = Console
+                        appender.console.name = STDOUT
+                        appender.console.layout.type = SimpleLayout
+                        rootLogger.level = debug
+                        rootLogger.appenderRef.stdout.ref = STDOUT
+                        """,
+                        config -> {
+                            LogHandler handler = config.getHandler("console");
+                            assertInstanceOf(SimpleLayout.class, ((ConsoleHandler) handler).getLayout());
+                        }
+                ),
+                new TestCase("Syslog Appender", """
+                        appender.syslog.type = Syslog
+                        appender.syslog.name = Syslog
+                        appender.syslog.host = localhost
+                        appender.syslog.port = 514
+                        appender.syslog.protocol = UDP
+                        rootLogger.level = debug
+                        rootLogger.appenderRef.syslog.ref = Syslog
+                        """,
+                        config -> {
+                            // Currently unsupported, should not crash and maybe return no handler or a default one
+                        }
+                ),
+                new TestCase("Complex Policies", """
+                        appender.rolling.type = RollingFile
+                        appender.rolling.name = RollingFile
+                        appender.rolling.fileName = build/rolling.log
+                        appender.rolling.filePattern = build/rolling-%d{MM-dd-yy-HH-mm-ss}-%i.log.gz
+                        appender.rolling.policies.type = Policies
+                        appender.rolling.policies.time.type = TimeBasedTriggeringPolicy
+                        appender.rolling.policies.time.interval = 2
+                        appender.rolling.policies.time.modulate = true
+                        appender.rolling.policies.size.type = SizeBasedTriggeringPolicy
+                        appender.rolling.policies.size.size = 100MB
+                        appender.rolling.strategy.type = DefaultRolloverStrategy
+                        appender.rolling.strategy.max = 5
+                        rootLogger.level = info
+                        rootLogger.appenderRef.rolling.ref = RollingFile
+                        """,
+                        config -> {
+                            LogHandler handler = config.getHandler("rolling");
+                            assertNotNull(handler);
+                            assertInstanceOf(RotatingFileHandler.class, handler);
                         }
                 )
         );
