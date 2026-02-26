148731b2d7156c8f4e88a34e35ed7e7a829e0df4
Axel Howind <axh@dua3.com>
2026-02-26 12:41:06 +0100

    Remove `SharableString` and `SharedString` classes along with associated tests.

--- src/test/java/org/slb4j/support/SupportTest.java
+++ src/test/java/org/slb4j/support/SupportTest.java
@@ -17,47 +17,9 @@ package org.slb4j.support;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.function.Supplier;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class SupportTest {
-
-    @Test
-    void testSharableAndSharedString() {
-        String base = "hello world";
-        SharableString ss = new SharableString(base);
-
-        assertEquals(base.length(), ss.length());
-        assertEquals('h', ss.charAt(0));
-        assertEquals(base, ss.toString());
-        assertEquals(base.hashCode(), ss.hashCode());
-        assertEquals(new SharableString(base), ss);
-
-        SharedString sub = ss.subSequence(6, 11);
-        assertEquals("world", sub.toString());
-        assertEquals(5, sub.length());
-        assertEquals('w', sub.charAt(0));
-
-        SharedString subSub = sub.subSequence(1, 4);
-        assertEquals("orl", subSub.toString());
-
-        // Test equality and hashcode
-        SharedString anotherWorld = new SharableString("other world").subSequence(6, 11);
-        assertEquals("world", anotherWorld.toString());
-        assertEquals(sub, anotherWorld);
-        assertEquals(sub.hashCode(), anotherWorld.hashCode());
-    }
-
-    @Test
-    void testSharableStringIndexOf() {
-        SharableString ss = new SharableString("hello world");
-        assertEquals(2, ss.indexOf('l', 0));
-        assertEquals(3, ss.indexOf('l', 3));
-        assertEquals(9, ss.indexOf('l', 4));
-        assertEquals(-1, ss.indexOf('z', 0));
-    }
 
     @Test
     void testAnsiCode() {
