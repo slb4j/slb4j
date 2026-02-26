01769cfba225f227c41d8e13c7d709062666791e
Axel Howind <axh@dua3.com>
2026-02-26 17:36:15 +0100

    Optimize millisecond formatting in `TimeOnlyTimeStampFormatter` by improving variable usage and reducing redundant assignments.

--- src/main/java/org/slb4j/support/formatter/TimeOnlyTimeStampFormatter.java
+++ src/main/java/org/slb4j/support/formatter/TimeOnlyTimeStampFormatter.java
@@ -38,26 +38,26 @@ public final class TimeOnlyTimeStampFormatter extends AbstractTimeStampFormatter
      * Constructs an {@code ISO8601TimeStampFormatter} with custom separators for date-time and milliseconds,
      * and an optional time zone.
      *
-     * @param millisSeparator   the character used to separate the seconds and milliseconds components in the formatted timestamp
-     * @param inlcudeOffset     whether to include the time zone offset in the formatted timestamp
-     * @param zoneId            the {@link ZoneId} used for formatting the timestamp, or {@code null} to default to the system time zone
+     * @param millisSeparator the character used to separate the seconds and milliseconds components in the formatted timestamp
+     * @param zoneId          the {@link ZoneId} used for formatting the timestamp, or {@code null} to default to the system time zone
      */
-    public TimeOnlyTimeStampFormatter(char millisSeparator, boolean inlcudeOffset, @Nullable ZoneId zoneId) {
+    public TimeOnlyTimeStampFormatter(char millisSeparator, @Nullable ZoneId zoneId) {
         super(zoneId == null ? ZoneId.systemDefault() : zoneId);
         this.millisSeparator = millisSeparator;
     }
 
     @Override
     protected void appendTo(Appendable app, int y, int M, int d, int H, int m, int s, int S, int offsetSeconds) throws IOException {
+        int q1 = S / 100;
+        int q2 = S % 100;
+
         app.append(DIGIT_TENS[H]).append(DIGIT_ONES[H]);
         app.append(':');
         app.append(DIGIT_TENS[m]).append(DIGIT_ONES[m]);
         app.append(':');
         app.append(DIGIT_TENS[s]).append(DIGIT_ONES[s]);
         app.append(millisSeparator);
-        int q1 = S / 100;
         app.append(DIGIT_ONES[q1]);
-        q1 = S % 100;
-        app.append(DIGIT_TENS[q1]).append(DIGIT_ONES[q1]);
+        app.append(DIGIT_TENS[q2]).append(DIGIT_ONES[q2]);
     }
 }
